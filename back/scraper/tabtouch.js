0079013006a8fab2100d85cdeadb9b1e74a57859
sceptre520 <denysshowdev@gmail.com>
2021-08-25 18:57:53 +0300

    final-callback-save

--- back/scraper/tabtouch.js
+++ back/scraper/tabtouch.js
@@ -207,6 +207,6 @@ exports.run = () => {
     for(var tmp_i=0; tmp_i<tmp_len; tmp_i++) {
         setTimeout( function (tmp_i) {
             getData(vgmObjs[tmp_i])
-        }, 100*tmp_i, tmp_i)
+        }, 1000*tmp_i, tmp_i)
     }
 }
